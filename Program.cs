6e2b77b7c40dde683ba626869084e3b1099f6da2
Vijaybhai Desai <47266239+Desaivijay@users.noreply.github.com>
2023-08-18 17:55:11 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using TMDBLibrary.Data;
+
+namespace TMDB_App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            // Add DbContext
+            builder.Services.AddDbContext<MovieContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            // Register TMDBService with HttpClient
+            builder.Services.AddHttpClient<TMDBLibrary.Services.TMDBService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Movies}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
